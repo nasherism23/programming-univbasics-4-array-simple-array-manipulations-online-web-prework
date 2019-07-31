1b1d4253c7143f3010ee8628c535d968db866e3b
James Nash <nash.jamesm@gmail.com>
2019-07-31 15:41:22 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -28,4 +28,21 @@ def using_concat(array1, array2)
 end
 
 def using_insert(array, element)
-  array.insert()
+   array.insert(4, item)
+end
+
+def using_uniq(array)
+  array.uniq 
+end
+
+def using_flatten(array)
+  array.flatten 
+end
+
+def using_delete(array, string)
+  array.delete(string)
+end
+
+def using_delete_at(array, num)
+  array.delete_at(num)
+end
